2177671e02235b9133c68e7b2aef1bc27233cee0
Denis Krinitsyn <skylinekrin@gmail.com>
2022-11-30 22:52:05 +0500

    Task2 done

---
+++ Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число 1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2) 
+{
+    Console.WriteLine("max = {0}, min = {1}", num1, num2);
+}
+else if (num1 < num2)
+{
+    Console.WriteLine("max = {0}, min = {1}", num2, num1);
+}
+else if (num1 == num2)
+{
+    Console.WriteLine("max = {0}", num1);
+}
+else
+{
+    Console.WriteLine("Введены неккоректные значения");
+}
