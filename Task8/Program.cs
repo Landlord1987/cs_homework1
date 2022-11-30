1bd64f5bd8d69af8890002a2ac9013d804d4d0e6
Denis Krinitsyn <skylinekrin@gmail.com>
2022-11-30 23:17:16 +0500

    Task8 done

---
+++ Task8/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 2; i <= num; i++)
+{
+    if (i%2 == 0)
+    {
+       Console.Write(i + " "); 
+    }
+}
